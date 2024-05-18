5768c9403f5bd313dee80176ed5841d34ddf1c1f
Rupesh3000 <rupeshpatil11121@gmail.com>
2024-05-18 22:33:58 +0000

    build UI of Quiz game....

---
+++ dist/pro_02/script.js
@@ -0,0 +1,30 @@
+const questionAndAns = [
+    {
+        quesion_1: "who is best super hero",
+        option_A: "Iron man",
+        option_B: "Tho",
+        option_C: "Cap",
+        option_D: "Black Panther"
+    }
+    ,
+    {
+        quesion_1: "who is best super hero",
+        option_A: "Iron man",
+        option_B: "Tho",
+        option_C: "Cap",
+        option_D: "Black Panther"
+    },
+    {
+        quesion_1: "who is best super hero",
+        option_A: "Iron man",
+        option_B: "Tho",
+        option_C: "Cap",
+        option_D: "Black Panther"
+    },
+    {
+        quesion_1: "who is best super hero",
+        option_A: "Iron man",
+        option_B: "Tho",
+        option_C: "Cap",
+        option_D: "Black Panther"
+    }];
