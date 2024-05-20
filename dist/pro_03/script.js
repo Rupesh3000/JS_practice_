f6ec602b4c0172496204ddefcff454b0ab3c5292
Rupesh3000 <rupeshpatil11121@gmail.com>
2024-05-20 16:36:54 +0000

    created ui of password generator

---
+++ dist/pro_03/script.js
@@ -0,0 +1,11 @@
+
+const passOut = document.getElementById("pass-out");
+const lengthNum = document.getElementById("length-num");
+const lowerCase = document.getElementById("lowercase");
+const upperCase = document.getElementById("uppercase");
+const number = document.getElementById("number");
+const symbol = document.getElementById("symbol");
+
+const copyBtn = document.getElementById("copy-btn");
+const GTbtn = document.getElementById("gt-btn");
+
