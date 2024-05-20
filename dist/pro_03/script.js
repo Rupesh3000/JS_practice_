93c91f0019101ea75bb114793ac44a0bfc20b6e3
Rupesh3000 <rupeshpatil11121@gmail.com>
2024-05-20 17:44:02 +0000

    add logic of generot password using JS

--- dist/pro_03/script.js
+++ dist/pro_03/script.js
@@ -1,4 +1,6 @@
 
+// select all input 
+
 const passOut = document.getElementById("pass-out");
 const lengthNum = document.getElementById("length-num");
 const lowerCase = document.getElementById("lowercase");
@@ -6,6 +8,46 @@ const upperCase = document.getElementById("uppercase");
 const number = document.getElementById("number");
 const symbol = document.getElementById("symbol");
 
+
+// select bottons 
+
 const copyBtn = document.getElementById("copy-btn");
 const GTbtn = document.getElementById("gt-btn");
 
+
+// add all upper and lower case letter , numbers and symbols
+
+const Upletter = "ABCDEFGHIKLMNOPQRSTUVWXYZ ";
+const LowLetter = "abcdefghiklmnopqrstuvwxyz ";
+const num = "0123456789";
+const allSym = "!@#$%^&*_-<>";
+
+
+GTbtn.addEventListener("click", () => {
+
+    let str = ""
+
+    if (upperCase.checked) {
+        str = str + Upletter;
+    }
+
+    if (lowerCase.checked) {
+        str = str + LowLetter;
+    }
+
+    if (number.checked) {
+        str = str + num;
+    }
+
+    if (symbol.checked) {
+        str = str + allSym;
+    }
+
+    let password = "";
+    for (let i = 0; i < lengthNum.value; i++) {
+        let index = Math.floor(Math.random() * str.length);
+        password += str[index];
+    }
+
+    passOut.value = password;
+});
