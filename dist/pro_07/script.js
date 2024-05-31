a9fb8e22f68944ef4b7750dd79fa9e3c2fb33c41
Rupesh3000 <rupeshpatil11121@gmail.com>
2024-05-31 21:13:02 +0000

    random quotes generator

--- dist/pro_07/script.js
+++ dist/pro_07/script.js
@@ -1,6 +1,40 @@
 
 const quote = document.querySelector(".quote");
 const author = document.querySelector(".author");
-const btn = document.querySelector("button");
+const nextBtn = document.querySelector("button");
+
+
+
+// using async and await
+
+
+/* 
+async function randomQuote() {
+    const response = await fetch('https://api.quotable.io/random')
+    const date = await response.json()
+
+    quote.innerHTML = date.content;
+    author.innerHTML = `- ${date.author}`
+
+
+} */
+
+function randomQuote() {
+    fetch('https://api.quotable.io/random')
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            quote.innerHTML = data.content;
+            author.innerHTML = `- ${data.author}`;
+        })
+        .catch(error => {
+            console.error('Error fetching the quote:', error);
+        });
+
+}
+
+
+nextBtn.addEventListener("click", randomQuote);
 
 
